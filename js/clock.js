e538e0315e49a46992e55453f502fdf2f3434869
OlesiaHolod <olesja.kholod@gmail.com>
2022-06-30 08:20:22 +0000

    clock

---
+++ js/clock.js
@@ -0,0 +1,13 @@
+(function() {
+    const clockContainer = document.querySelector('.clock');
+
+    function updateClock() {
+        clockContainer.innerText = new Date().toLocaleTimeString(); 
+    }
+    
+    setInterval(updateClock, 1000); 
+})()
+
+
+
+
