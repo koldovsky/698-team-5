e8bfbd07042352ed5efff4e916c5b83964c36271
AlexSilaev2022 <silaev.sasha2016@gmail.com>
2022-07-01 10:01:49 +0000

    fixed seven block

--- js/carousel__categories.js
+++ js/carousel__categories.js
@@ -1,45 +1,45 @@
 (function () {
 
     const slidesFood = [
-        `<div class="carrousel__categories-item">
-    <img class="carrousel__categories-vegeterian" src="img/vegeterian_cuisine.svg" alt="salad">
-    <h3 class="carrousel__categories-name">Vegeterian</h3>
-    <div class="carrousel__name-separator"></div>
-    <p class="carrousel__categories-comment">Order the best vegan and vegetarian meals with the
+        `<div class="carousel__categories-item">
+    <img class="carousel__categories-vegeterian" src="img/vegeterian_cuisine.svg" alt="salad">
+    <h3 class="carousel__categories-name">Vegeterian</h3>
+    <div class="carousel__name-separator"></div>
+    <p class="carousel__categories-comment">Order the best vegan and vegetarian meals with the
         fastest
         delivery possible.</p>
 </div>`,
-        `<div class="carrousel__categories-item">
-    <img class="carrousel__categories-pizza" src="img/pizza_cuisine.svg" alt="pizza">
-    <h3 class="carrousel__categories-name">Pizza</h3>
-    <div class="carrousel__name-separator"></div>
-    <p class="carrousel__categories-comment">We deliver original pizza from the best Italian
+        `<div class="carousel__categories-item">
+    <img class="carousel__categories-pizza" src="img/pizza_cuisine.svg" alt="pizza">
+    <h3 class="carousel__categories-name">Pizza</h3>
+    <div class="carousel__name-separator"></div>
+    <p class="carousel__categories-comment">We deliver original pizza from the best Italian
         restaurants
         in
         New York.</p>
 </div>`,
-        `<div class="carrousel__categories-item">
-    <img class="carrousel__categories-sushi" src="img/sushi_cuisine.svg" alt="sushi">
-    <h3 class="carrousel__categories-name">Sushi</h3>
-    <div class="carrousel__name-separator"></div>
-    <p class="carrousel__categories-comment">Delicious sushi from the best Japanese restaurants
+        `<div class="carousel__categories-item">
+    <img class="carousel__categories-sushi" src="img/sushi_cuisine.svg" alt="sushi">
+    <h3 class="carousel__categories-name">Sushi</h3>
+    <div class="carousel__name-separator"></div>
+    <p class="carousel__categories-comment">Delicious sushi from the best Japanese restaurants
         delivered to
         your place.</p>
 </div>`,
-        `<div class="carrousel__categories-item">
-    <img class="carrousel__categories-bakery" src="img/bakery_cuisine.svg" alt="cake">
-    <h3 class="carrousel__categories-name">Bakery</h3>
-    <div class="carrousel__name-separator"></div>
-    <p class="carrousel__categories-comment">Our couriers will deliver fresh and luscious
+        `<div class="carousel__categories-item">
+    <img class="carousel__categories-bakery" src="img/bakery_cuisine.svg" alt="cake">
+    <h3 class="carousel__categories-name">Bakery</h3>
+    <div class="carousel__name-separator"></div>
+    <p class="carousel__categories-comment">Our couriers will deliver fresh and luscious
         pastries from
         your
         favorite bakery.</p>
 </div>`,
-        `<div class="carrousel__categories-item">
-    <img class="carrousel__categories-fast-food" src="img/fast_food_cuisine.svg" alt="shawarma">
-    <h3 class="carrousel__categories-name">Fast Food</h3>
-    <div class="carrousel__name-separator"></div>
-    <p class="carrousel__categories-comment">Order your "guilty pleasure" and get it delivered
+        `<div class="carousel__categories-item">
+    <img class="carousel__categories-fast-food" src="img/fast_food_cuisine.svg" alt="shawarma">
+    <h3 class="carousel__categories-name">Fast Food</h3>
+    <div class="carousel__name-separator"></div>
+    <p class="carousel__categories-comment">Order your "guilty pleasure" and get it delivered
         to your
         place
         in just 30 minutes.</p>
@@ -49,7 +49,7 @@
     let slideFoodIndex = 0;
 
     function showSlides() {
-        const slideBox = document.querySelector('.carrousel__cuisine');
+        const slideBox = document.querySelector('.carousel__cuisine');
         slideBox.innerHTML = slidesFood[slideFoodIndex];
         if (window.matchMedia('(min-width: 520px)').matches) {
             const secondSlideFoodIndex = slideFoodIndex + 1 >= slidesFood.length ? 0 : slideFoodIndex + 1;
@@ -75,9 +75,9 @@
         showSlides();
     }
 
-    const nextButton = document.querySelector('.carrousel__categories-next-button');
+    const nextButton = document.querySelector('.carousel__categories-next-button');
     nextButton.addEventListener('click', nextSlidesFood);
-    const prevButton = document.querySelector('.carrousel__categories-prev-button');
+    const prevButton = document.querySelector('.carousel__categories-prev-button');
     prevButton.addEventListener('click', prevSlidesFood);
 
     window.addEventListener('resize', showSlides);
