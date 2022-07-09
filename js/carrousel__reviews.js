0f90ce5e63d6d6090453850d5b2f545f84565ccd
tstanya <tania.stashak@gmail.com>
2022-07-09 21:08:20 +0000

    Revert "Revert "Merge branch 'main' into solve_merge_issue""
    This reverts commit 4311a67fb7a4e01f5c72617cc4628eee7a88b1df.

--- js/carrousel__reviews.js
+++ js/carrousel__reviews.js
@@ -79,19 +79,30 @@
     function nextSlide() {
         slideIndex = slideIndex + 1 >= slides.length ? 0 : slideIndex + 1;
         showCurrentSlide();
+        dotActive(slideIndex);
     }
 
     function previousSlide() {
         slideIndex = slideIndex - 1 < 0 ? slides.length - 1 : slideIndex - 1;
         showCurrentSlide();
+        dotActive(slideIndex);
     }
 
-    //function showSlide(slideNum) {
-    //    slideIndex = slideNum;
-    //    showCurrentSlide();
-    //}
+    function dotActive(slideIndex) {
+        for(let i = 0; i < dots.length; i++)
+        {
+            dots[i].classList.remove("reviews-dot-active");
+        }
+        dots[slideIndex].classList.add("reviews-dot-active");
+    }
 
-    //const dots = document.querySelectorAll('.reviews-dots');
+    function showSlide(slideNum) {
+        slideIndex = slideNum;
+        showCurrentSlide();
+        dotActive(slideIndex);
+    }
+
+    const dots = document.querySelectorAll('.reviews-dot');
     const nextButton = document.querySelector('.carrousel__reviews > .carrousel__reviews-next-button');
     const previousButton = document.querySelector('.carrousel__reviews > .carrousel__reviews-previous-button');
 
@@ -99,11 +110,12 @@
     previousButton.addEventListener('click', previousSlide);
 
 
-    //for (let i = 0; i < dots.length; i++) {
-    //    dots.addEventListener('click', showSlide(i));
-    //  } 
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].addEventListener('click', () => showSlide(i));
+      } 
 
     window.addEventListener('resize', showCurrentSlide);
 
     showCurrentSlide();
+    dotActive(slideIndex);
 })()
